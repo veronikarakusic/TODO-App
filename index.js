82c887c35f651c295b408b2218c5adab0008fc35
veronikarakusic <vrakusicc@gmail.com>
2024-07-03 11:41:44 +0200

    added select and delete items

--- index.js
+++ index.js
@@ -1,12 +1,52 @@
 
 
 function insert(){
-    let text = document.getElementById('text');
-    let unos = document.getElementById('unos');
-
-    let uneseno = unos.value;
+    let text = document.getElementById("text");
+    let unos = document.getElementById("unos");
     let p = document.createElement("li");
-    p.textContent = uneseno;
-    text.appendChild(p);
+
+    let inserted = unos.value;
+
+    
+    p.textContent = inserted;
+    
+    
+    let importance = document.getElementById('importance').value;
+    
+
+    let most = document.getElementById('most');
+    let medium = document.getElementById('medium');
+    let least = document.getElementById('least');
+
+    important = most.value;
+    medium = medium.value;
+    least = least.value;
+
+    console.log(importance);
+
+    p.classList.add("important");
+ 
+
+    switch(importance){
+        case 'important':
+            
+            p.className="important";
+            break;
+        case 'medium':
+            
+            p.className="medium";
+            break;
+        case 'least':
+            
+            p.className="least";
+            break;
+
+}
+p.onclick = function(){
+    text.removeChild(p);
+}
+        text.appendChild(p);
+
+        unos.value = '';
     
 }
